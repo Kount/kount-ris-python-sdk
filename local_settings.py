263b912ad7092d6e76e786eca1eb42c623b64343
danibudi <d.budinova@gmail.com>
2017-05-30 14:05:49 +0300

    KS-55 mock of local_settings.py

---
+++ local_settings.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This file is part of the Kount python sdk project https://github.com/Kount/kount-ris-python-sdk/)
+# Copyright (C) 2017 Kount Inc. All Rights Reserved.
+
+kount_api_key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI5OTk2NjYiLCJhdWQiOiJLb3VudC4xIiwiaWF0IjoxNDk0NTM0Nzk5LCJzY3AiOnsia2EiOm51bGwsImtjIjpudWxsLCJhcGkiOmZhbHNlLCJyaXMiOnRydWV9fQ.eMmumYFpIF-d1up_mfxA5_VXBI41NSrNVe9CyhBUGck"
+kount_api_key999667 = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI5OTk2NjciLCJhdWQiOiJLb3VudC4xIiwiaWF0IjoxNDk0NTM1OTE2LCJzY3AiOnsia2EiOm51bGwsImtjIjpudWxsLCJhcGkiOmZhbHNlLCJyaXMiOnRydWV9fQ.KK3zG4dMIhTIaE5SeCbej1OAFhZifyBswMPyYFAVRrM"
+
+url_api = "https://risk.beta.kount.net"
+url_api_beta = url_api
+
+#~ replace the mock salt with the salt provided by Kount
+salt = "very secret salt provided by Kount"
+
+#~ request timeout in seconds
+timeout = 5
+
+#~ raise_errors - if  True - raise errors instead of logging in debugger
+raise_errors = False
+
+merchant_id = '999666'
+merchant_id_999667 = '999667'
+ptok = "0007380568572514"
