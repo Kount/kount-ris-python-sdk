9c1c33d2b3065f9beeb55efd8db844b570c73287
danibudi <d.budinova@gmail.com>
2017-06-01 16:26:43 +0300

    ks-55 requirements.txt

---
+++ kount/requirements.py
@@ -0,0 +1,14 @@
+#~ uncomment this to install pytest
+#~ pytest==3.0.6
+#~ pytest-html==1.14.2
+#~ pytest-metadata==1.5.0
+
+requests==2.11.1
+
+simplejson==3.10.0
+
+#~ uncomment this to install Sphinx to generate the documentation
+#~ Sphinx==1.6.2
+#~ sphinx-autobuild==0.6.0
+#~ sphinxcontrib-websupport==1.0.1
+
